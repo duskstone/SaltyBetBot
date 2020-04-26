88a93df9b5c9686fb922692cb69c8f0f44de6bcd
Jonathan <yoniyaniv@gmail.com>
2020-04-26 11:58:51 -0400

    git push

--- app/controllers/bet_pools_controller.rb
+++ app/controllers/bet_pools_controller.rb
@@ -23,6 +23,18 @@ class BetPoolsController < ApplicationController
     end
 
     def update
+        # ********** UPDATE: HOW WINNER IS DETERMINED ***********
+        # the parameter 'winner' tells us if the PLAYER has won or lost,
+        # based on that we need to determine if the value of bet_pools.winner
+        # should be set to the user's bet.action or the opposite.
+        #
+        # i.e. player1: !bet matrix is real 500 true
+        #      player1: !bet lost matrix is real
+        #
+        # in this scenario the bet_pool of title: 'matrix is real' should have
+        # its winner column set to FALSE because it is the opposit of player1's bet
+        # *******************************************************
+
         @bet_pool = BetPool.find_by(title: bet_pool_params[:title])
         @user = User.find_by(username: user_params[:username])
 
@@ -31,9 +43,11 @@ class BetPoolsController < ApplicationController
                 winner: bet_pool_params[:winner]
             )
             unless @bet_pool.winner.nil?
+                # ************ UPDATE: DISTRIBUTE WINNINGS  *************
                 # @bet = Bet.find_by(user_id: @user.id, bet_pool_id: @bet_pool.id)
                 # add users to array based on what their action was (true/false)
                 # distribute pool (.total_salt) by dividing by array length
+                # *******************************************************
 
                 render json: {message: "The pool is closed, #{@bet_pool.winner} wins!"}
             end
